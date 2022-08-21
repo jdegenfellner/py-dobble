3c1bd48d3341a6bd00ca04c5857635ef25bef7ab
jdegenfellner <jdegenfellner@gmail.com>
2022-08-21 23:03:49 +0200

    excel files with cards and symbols uploaded

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 def print_hi(name):
-
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+import openpyxl
+import openpyxl
+from pathlib import Path
 
-    print_hi('PyCharm')
+xlsx_file = Path('SimData', 'play_data.xlsx')
+wb_obj = openpyxl.load_workbook(xlsx_file)
+
+# Read the active sheet:
+sheet = wb_obj.active
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
