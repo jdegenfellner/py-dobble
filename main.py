001918120306f731bcc079a2fcbad84441ad0769
jdegenfellner <jdegenfellner@gmail.com>
2022-08-21 23:29:12 +0200

    excel files with cards and symbols uploaded reading excel file with main.py

--- main.py
+++ main.py
@@ -2,21 +2,14 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 import openpyxl
-import openpyxl
-from pathlib import Path
+import pandas as pd
 
-xlsx_file = Path('SimData', 'play_data.xlsx')
-wb_obj = openpyxl.load_workbook(xlsx_file)
+wb_obj = openpyxl.load_workbook('Cards_Symbols.xlsx')
 
 # Read the active sheet:
 sheet = wb_obj.active
-
+data_df = pd.DataFrame(sheet.values)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
