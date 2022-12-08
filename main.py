34b4523094405b89e27759e760cdf569ea832242
jdegenfellner <jdegenfellner@gmail.com>
2022-12-08 23:11:01 +0100

    deleting graphical output for now

--- main.py
+++ main.py
@@ -82,16 +82,3 @@ elif count_player_2 > count_player_1:
 else:
     print("Result: draw")
 
-
-
-
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
